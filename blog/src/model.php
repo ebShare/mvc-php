020ccdd431cec32ff96334f53320d32b79388f46
ebShare <127939817+ebShare@users.noreply.github.com>
2023-10-18 05:13:47 +0200

    Bas pour afficher les commentaires

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,26 +1,25 @@
 <?php
-function getPosts()
-{
-    //We connect to the database.
-    try{
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','blog','password');
-    }          
-    catch(Exception $e){
-        die('Erreur :'.$e->getMessage());
+
+function getPosts() {
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
     }
-    //We retrieve the 5 last blog posts.
-    $statement=$database->query(
-        "SELECT id, titre, contenu, DATE_FORMAT(creation_date,'%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY creation_date DESC LIMIT 0,5"
+
+    $statement = $database->query(
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
     $posts = [];
-    while(($row = $statement->fetch())){
+    while (($row = $statement->fetch())) {
         $post = [
-            'title'=> $row['title'],
-            'french_creation_date'=> $row['date_creation_fr'],
-            'content'=> $row['content'],
+            'title' => $row['title'],
+            'french_creation_date' => $row['french_creation_date'],
+            'content' => $row['content'],
         ];
+
         $posts[] = $post;
     }
+
     return $posts;
 }
-
