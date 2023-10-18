5a67ff2a1453e5207ac36b6747fe06e3f74cc94d
eb504530 <eb504530@INFO-NEV2-22-16>
2023-10-18 13:52:01 +0200

    Base pour créer un template

--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -22,9 +21,7 @@
                 <p>
                     <?= nl2br(htmlspecialchars($post['content'])); ?>
                     <br />
-                    <em>
-                        <a href="post.php?id=<?=urlencode($post['identifier'])?>">Commentaires</a>
-                    </em>
+                    <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
                 </p>
             </div>
         <?php
