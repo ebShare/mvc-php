d1c142a8e0827106c6f35e612b1981a9bd2ecf08
eb504530 <eb504530@INFO-NEV2-22-16>
2023-10-18 14:32:43 +0200

    Créez un routeur

---
+++ blog/src/controllers/homepage.php
@@ -0,0 +1,6 @@
+<?php
+require_once('src/model.php');
+function homepage(){
+    $posts = getPosts();
+    require('templates/homepage.php');
+}
---
+++ blog/src/controllers/post.php
@@ -0,0 +1,7 @@
+<?php
+require_once('src/model.php');
+function post(string $identifier){
+    $post = getPost($identifier);
+    $comments = getComments($identifier);
+    require('templates/post.php');
+}
