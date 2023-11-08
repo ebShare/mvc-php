fbeb9a79d3dc7a7b57e7fb9de25272e5eae1ad30
Elise BLIN <eb504530@iut21.u-bourgogne.fr>
2023-11-08 11:49:04 +0100

    Donnez vie à vos structures

--- blog/src/model/post.php
+++ blog/src/model/post.php
@@ -5,8 +5,9 @@ class Post{
     public $content;
     public $identifier;
 }
-class          PostRepository{
-    function getPosts() {
+class PostRepository{
+    function getPosts(PostRepository $repository) {
+        public $database = null;
         $database = dbConnect();
         $statement = $database->query(
             "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
@@ -26,29 +27,24 @@ class          PostRepository{
         return $posts;
     }
     
-    function getPost($identifier) {
-        $database = dbConnect();
-        $statement = $database->prepare(
-            "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts WHERE id = ?"
-        );
+    public function getPost($identifier) {
+        dbConnect($this);
+        $statement = $this->database->prepare("SELECT id, title, content,DATE_FORMAT(creation_date,'%d/%m/%Y à %Hh%imin%ss')AS french_creation_date FROM posts WHERE id = ?");
         $statement->execute([$identifier]);
-    
         $row = $statement->fetch();
-        $post = [
-            'title' => $row['title'],
-            'french_creation_date' => $row['french_creation_date'],
-            'content' => $row['content'],
-            'identifier' => $row['id'],
-        ];
-    
+        $post = new Post();
+        $post->title = $row['title'];
+        $post->frenchCreationDate = $row['french_creation_date'];
+        $post->content = $row['content'];
+        $post->identifier = $row['id'];
         return $post;
     }
     
-    function dbConnect()
+    function dbConnect(PostRepository $repository)
     {
-        $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-    
-        return $database;
+        if ($repository->database === null) {
+            $repository->database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8','blog','password');
+        }
     }
     
 
